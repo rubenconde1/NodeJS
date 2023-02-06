f033777600fe043d1531ff85ea8c8664dfc08ede
rubenconde1 <rubencnd96@gmail.com>
2023-02-06 13:26:11 +0100

    Instalacion dependendicas, nueva ruta users/cool

--- Tutorial/express-locallibrary-tutorial/app.js
+++ Tutorial/express-locallibrary-tutorial/app.js
@@ -1,7 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
